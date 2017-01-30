84a1014f45b177e277e41fab5a2e958a8d03d208
Jack Wright <jackrwright1@gmail.com>
2017-01-30 07:03:56 -0500

    Single animation working.

--- SceneKitTest2/GameViewController.swift
+++ SceneKitTest2/GameViewController.swift
@@ -11,12 +11,14 @@ import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
+	
+	var jumpInPlaceAnimation = CAAnimation()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/human.dae")!
         
         // create and add a camera to the scene
         let cameraNode = SCNNode()
@@ -24,13 +26,13 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(cameraNode)
         
         // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
+        cameraNode.position = SCNVector3(x: 0, y: 10, z: 30)
+		
         // create and add a light to the scene
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
         lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        lightNode.position = SCNVector3(x: 0, y: 20, z: 20)
         scene.rootNode.addChildNode(lightNode)
         
         // create and add an ambient light to the scene
@@ -40,12 +42,26 @@ class GameViewController: UIViewController {
         ambientLightNode.light!.color = UIColor.darkGray
         scene.rootNode.addChildNode(ambientLightNode)
         
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+        // retrieve the human's node
+        let humanArmature = scene.rootNode.childNode(withName: "Armature", recursively: true)!
+		
+//		cameraNode.constraints = [SCNLookAtConstraint(target: humanArmature)]
         
         // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
+//        humanArmature.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+		
+		jumpInPlaceAnimation = CAAnimation.animationWithSceneNamed("art.scnassets/jumpInPlace.dae")!
+		humanArmature.addAnimation(jumpInPlaceAnimation, forKey: "jump")
+		
+		// Add a floor
+		let floorGeom = SCNFloor()
+		let floorMaterial = SCNMaterial()
+		floorMaterial.diffuse.contents = UIColor(red: 0.1, green: 0.5, blue: 0.1, alpha: 1.0)
+		floorMaterial.specular.contents = UIColor.black
+		floorGeom.firstMaterial = floorMaterial
+		let floorNode = SCNNode(geometry: floorGeom)
+		scene.rootNode.addChildNode(floorNode)
+		
         // retrieve the SCNView
         let scnView = self.view as! SCNView
         
